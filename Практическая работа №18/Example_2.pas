b44e644c0db3633dd094ec4bc5430112539eee72
Swerrio <120246505+Swerrio@users.noreply.github.com>
2024-03-04 13:12:32 +0530

    Добавил №18

---
+++ Практическая работа №18/Example_2.pas
@@ -0,0 +1,9 @@
+﻿program Example_2;
+var x,y:integer;
+begin
+  writeln('Введите два числа');
+  readln(x,y);
+  if x>y
+  then writeln(x) {если (IF) больше y, то (then)выводим x}
+  else writeln(y) {иначе (else) выводим y}
+end.
